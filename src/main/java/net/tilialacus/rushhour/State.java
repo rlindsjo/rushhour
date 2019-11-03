f9254cf71588be2a4322b3132201940909a506be
Roger Lindsjö <roger@tilialacus.net>
2019-11-03 22:29:45 +0100

    Build new valid states

--- src/main/java/net/tilialacus/rushhour/State.java
+++ src/main/java/net/tilialacus/rushhour/State.java
@@ -1,12 +1,41 @@
 package net.tilialacus.rushhour;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class State {
 
     private static final int SIZE = 6;
+    private static final char EMPTY = 0;
+    private final State parent;
     private char[] data = new char[SIZE * SIZE];
+
+    public State() {
+        this.parent = null;
+    }
+
+    private State(State parent, int x , int y, int dx, int dy) {
+        this.parent = parent;
+        System.arraycopy(parent.data, 0, data, 0, data.length);
+
+        char c = get(x + dx, y + dy);
+        do {
+            set(x,y, c);
+            x += dx;
+            y += dy;
+        } while (get(x, y) != c);
+        set(x, y, EMPTY);
+    }
 
     public void set(int x, int y, char type) {
         data[x+y*SIZE] = type;
+    }
+
+    public char get(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+            return 0;
+        }
+        return data[x+y*SIZE];
     }
 
     @Override
@@ -15,7 +44,7 @@ public class State {
         for (int y = 0; y < SIZE; y++) {
             for (int x = 0; x < SIZE; x++) {
                 char c = data[x + y * SIZE];
-                if (c >= 'A' && c <= 'Z') {
+                if (c != EMPTY) {
                     b.append(c);
                 } else {
                     b.append(' ');
@@ -24,5 +53,32 @@ public class State {
             b.append('\n');
         }
         return b.toString();
+    }
+
+    public Set<State> options() {
+        HashSet<State> states = new HashSet<>();
+        for (int y = 0; y < SIZE; y++) {
+            for (int x = 0; x < SIZE; x++) {
+                if (data[x + y * SIZE] == 0) {
+                    if (get(x-1, y) != EMPTY) {
+                        State newState = new State(this, x, y, -1, 0);
+                        states.add(newState);
+                    }
+                    if (get(x + 1, y) != EMPTY) {
+                        State newState = new State(this, x, y, 1, 0);
+                        states.add(newState);
+                    }
+                    if (get(x, y - 1) != EMPTY) {
+                        State newState = new State(this, x, y, 0, -1);
+                        states.add(newState);
+                    }
+                    if (get(x, y + 1) != EMPTY) {
+                        State newState = new State(this, x, y, 0, 1);
+                        states.add(newState);
+                    }
+                }
+            }
+        }
+        return states;
     }
 }
--- src/test/java/net/tilialacus/rushhour/StateTest.java
+++ src/test/java/net/tilialacus/rushhour/StateTest.java
@@ -2,6 +2,10 @@ package net.tilialacus.rushhour;
 
 import org.junit.Test;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,5 +16,23 @@ public class StateTest {
         State state = new State();
         state.set(1,2 , 'A');
         assertThat(state.toString(), is("      \n      \n A    \n      \n      \n      \n"));
+    }
+
+    @Test
+    public void freeMove() {
+        State state = new State();
+        state.set(1,2 , 'A');
+        state.set(0,2 , 'B');
+
+        Set<State> options = state.options();
+
+        assertThat(options.stream().map(State::toString).collect(Collectors.toSet()),
+                hasItems(
+                        "      \n      \nB A   \n      \n      \n      \n",
+                        "      \n A    \nB     \n      \n      \n      \n",
+                        "      \n      \nB     \n A    \n      \n      \n",
+                        "      \nB     \n A    \n      \n      \n      \n",
+                        "      \n      \n A    \nB     \n      \n      \n"
+                        ));
     }
 }
